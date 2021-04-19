f97baa082c88fe3f280e03cc7c2fc7e423c4e562
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-04-19 14:16:01 +0300

    🥶

--- src/api/src/database.ts
+++ src/api/src/database.ts
@@ -9,24 +9,25 @@ const pool = new Pool({
   password: process.env.POSTGRES_PASSWORD
 });
 
-pool.connect().catch(console.error);
+pool.connect().catch(reason => {
+  console.error(reason);
+  process.exit(1);
+});
 
-async function drones(): Promise<Drone[]> {
+export async function drones(): Promise<Drone[]> {
   const result = await pool.query('SELECT * FROM drones');
 
   return result.rows;
 }
 
-async function users(): Promise<User[]> {
+export async function users(): Promise<User[]> {
   const result = await pool.query('SELECT * FROM users');
 
   return result.rows;
 }
 
-async function pictures(): Promise<Picture[]> {
+export async function pictures(): Promise<Picture[]> {
   const result = await pool.query('SELECT * FROM pictures');
 
   return result.rows;
 }
-
-export default { drones, users, pictures };
