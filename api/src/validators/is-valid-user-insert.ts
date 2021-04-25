5c26d255d13d75414d636a07103e84d0f5175ed6
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-04-26 00:34:36 +0300

    clean code 🧹

--- api/src/validators/is-valid-user-insert.ts
+++ api/src/validators/is-valid-user-insert.ts
@@ -1,6 +1,26 @@
 import { UserInsertModel } from '../models';
-import { isSameStructure } from './common';
 
+
+function isSameStructure(a: object, b: object): boolean {
+  const aEntries = Object.entries(a).sort(([aName], [bName]) => aName.localeCompare(bName));
+  const bEntries = Object.entries(b).sort(([aName], [bName]) => aName.localeCompare(bName));
+
+  if (aEntries.length !== bEntries.length)
+    return false;
+
+  for (let i = 0; i < aEntries.length; ++i) {
+    const aEntry = aEntries[i];
+    const bEntry = bEntries[i];
+
+    if (aEntry[0] !== bEntry[0])
+      return false;
+
+    if (typeof aEntry[1] !== typeof bEntry[1])
+      return false;
+  }
+
+  return true;
+}
 
 export function isValidUserInsert(x: unknown): x is UserInsertModel {
   if (typeof x !== 'object')
--- api/src/validators/is-valid-user-update.ts
+++ api/src/validators/is-valid-user-update.ts
@@ -1,11 +1,19 @@
 import { UserUpdateModel } from '../models';
 
 
+export function hasValidPropertyNames(obj: object, names: string[]): boolean {
+  return Object.getOwnPropertyNames(obj).every(n => names.includes(n));
+}
+
+
 export function isValidUserUpdate(x: unknown): x is UserUpdateModel {
   if (typeof x !== 'object')
     return false;
 
   if (x === null)
+    return false;
+
+  if (!hasValidPropertyNames(x, ['name', 'username']))
     return false;
 
   return true;
